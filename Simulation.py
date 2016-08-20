5a3acb61226ab41d5b3d08eb43e287ece573feea
Marlan Perumal <marlan.perumal@gmail.com>
2016-08-20 16:32:38 +0200

    minor changes to Simulation

--- Simulation.py
+++ Simulation.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 
 
 class HandProbability(object):
-    def __init__(self, num_players=2, default_repeats=1000):
-        self.num_players = num_players
+    def __init__(self, num_players=None, default_repeats=1000):
+        if num_players is None:
+            self.num_players = 2
+        else:
+            self.num_players = num_players
         self.default_repeats = default_repeats
 
-    def run(self, my_original_cards=None, community_cards=None, repeats=None, verbose=False):
+    def run(self, my_hole_cards=None, community_cards=None, repeats=None, verbose=False):
         if repeats is None:
             repeats = self.default_repeats
 
-        if my_original_cards is None:
-            my_original_cards = []
+        if my_hole_cards is None:
+            my_hole_cards = []
 
         if community_cards is None:
             community_cards = []
@@ -32,10 +35,10 @@ class HandProbability(object):
         for _ in range(repeats):
             result = "win"
             deck = Deck()
-            deck.extract(my_original_cards)
+            deck.extract(my_hole_cards)
             deck.extract(community_cards)
             deck.shuffle()
-            my_cards = my_original_cards + deck.deal(2 - len(my_original_cards))
+            my_cards = my_hole_cards + deck.deal(2 - len(my_hole_cards))
             remaining_community_cards = deck.deal(7 - len(my_cards) - len(community_cards))
             my_hand = Hand(my_cards + community_cards + remaining_community_cards)
             my_hand.calc_rank()
@@ -62,7 +65,7 @@ class HandProbability(object):
         if verbose:
             print("Hand Probability Simulation")
             print("Playing against {} other player(s)".format(self.num_players - 1))
-            print("With hand: ", my_original_cards)
+            print("With hand: ", my_hole_cards)
             print("And Community Cards: ", community_cards)
             print("Over {} rounds".format(repeats))
             print("*****************************************")
@@ -70,6 +73,7 @@ class HandProbability(object):
             print("Draw Percentage: {0:.0%}".format(record["draw"] / record["total"]))
             print("Loss Percentage: {0:.0%}".format(record["loss"] / record["total"]))
             print("Relative Hand Strength: {0:.2f}".format(stack/repeats))
+
         record["win percentage"] = record["win"] / record["total"]
         record["draw percentage"] = record["draw"] / record["total"]
         record["loss percentage"] = record["loss"] / record["total"]
@@ -85,10 +89,10 @@ if __name__ == "__main__":
         unsuited_probability = []
         suited_probability = []
         for rank_2 in rank_list[:i]:
-            suited_record = simulation.run(my_original_cards=[Card(rank_1, "Hearts"), Card(rank_2, "Hearts")])
+            suited_record = simulation.run(my_hole_cards=[Card(rank_1, "Hearts"), Card(rank_2, "Hearts")])
             suited_probability.append(suited_record["win percentage"] + suited_record["draw percentage"])
         for rank_2 in rank_list[i:]:
-            unsuited_record = simulation.run(my_original_cards=[Card(rank_1, "Hearts"), Card(rank_2, "Spades")])
+            unsuited_record = simulation.run(my_hole_cards=[Card(rank_1, "Hearts"), Card(rank_2, "Spades")])
             unsuited_probability.append(unsuited_record["win percentage"] + unsuited_record["draw percentage"])
         probability_matrix.append(suited_probability + unsuited_probability)
     print(rank_list)
